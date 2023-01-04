e875f1869b8a204d7df987d9f3100c25d5dd455a
mpayne382 <mark.payne@te.com>
2023-01-04 11:20:01 +0000

    Added src folder

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public int generatePrime() {
+        return 13;
+    }
+
+    public static void main(String... args) {
+        Main primeGenerator = new Main();
+        System.out.println(primeGenerator.generatePrime());
+    }
+
+}
